6123b76720a7512ad3372388f541b4b2a7f20c32
MD Milon <milon@kali>
2024-12-21 22:16:16 +0600

    add flowbite and custom tailwind container breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+import flowbite from "flowbite-react/tailwind";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true, // Center the container (optional)
+        padding: "1rem", // Add padding (optional)
+        screens: {
+          sm: "100%", // Full width for small screens
+          md: "640px", // Adjust width for medium screens
+          lg: "800px", // Less width for large screens
+          xl: "960px", // Less width for extra large screens
+          "2xl": "1120px", // Less width for 2XL screens
+        },
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), flowbite.plugin()],
 };
